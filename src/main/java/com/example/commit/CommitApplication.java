6c176f83747c846bfbe2bbd69c5a872d697cdc42
Ji Seungmin <cswcsm02@gmail.com>
2023-08-21 14:55:04 +0900

    [feat]: 자 이제 됐다!

--- src/main/java/com/example/commit/CommitApplication.java
+++ src/main/java/com/example/commit/CommitApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommitApplication {
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommitApplication.class, args);
