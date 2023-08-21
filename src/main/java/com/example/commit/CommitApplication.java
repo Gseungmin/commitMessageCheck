ef2bab401aa870480b349f4900a21d1bbf53c743
Ji Seungmin <cswcsm02@gmail.com>
2023-08-21 20:25:09 +0900

    최종커밋

--- src/main/java/com/example/commit/CommitApplication.java
+++ src/main/java/com/example/commit/CommitApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommitApplication {
-	
-
 	public static void main(String[] args) {
 		SpringApplication.run(CommitApplication.class, args);
 	}
-
 }
